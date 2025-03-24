ed0d997cae20f9ac0d7f51eae8cf2c608c2576a7
hannanhanif786 <muhammadhannanhanif1122@gmail.com>
2025-03-24 13:10:20 +0500

    setup project

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("markflow.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+]
